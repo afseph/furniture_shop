6ee249679a8c812279e0e979d9c9a75e4e8929b0
afseph <afseph@gmail.com>
2025-04-08 22:06:24 +0300

    ADD: Route to get product by ID

--- backend/app/products/router.py
+++ backend/app/products/router.py
@@ -12,3 +12,10 @@ router = APIRouter(prefix='/products', tags=['Products endpoints'])
 @router.get('/', summary="Получить все товары")
 async def get_all_products(request_body: RBProduct = Depends()) -> list[SProducts]:
     return await ProductDAO.get_all(**request_body.to_dict())
+
+@router.get("/{id}", summary="Получить одного студента по id")
+async def get_product_by_id(product_id: int) -> SProducts | None:
+    rez = await ProductDAO.find_one_or_none_by_id(product_id)
+    if rez is None:
+        return {'message': f'Студент с ID {product_id} не найден!'}
+    return rez
