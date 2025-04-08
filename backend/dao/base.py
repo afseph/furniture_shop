85778f3c9fe825f168a973af608e423fad17b3aa
afseph <afseph@gmail.com>
2025-04-08 22:20:34 +0300

    ADD: BaseDAO Method that allows to update model data

--- backend/dao/base.py
+++ backend/dao/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy.future import select
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import update as sqlalchemy_update, delete as sqlalchemy_delete
 
 from app.database import async_session_maker
 
@@ -32,3 +33,21 @@ class BaseDAO:
                     await session.rollback()
                     raise e
                 return new_instance
+            
+    @classmethod
+    async def update(cls, filter_by, **values):
+        async with async_session_maker() as session:
+            async with session.begin():
+                query = (
+                    sqlalchemy_update(cls.model)
+                    .where(*[getattr(cls.model, k) == v for k, v in filter_by.items()])
+                    .values(**values)
+                    .execution_options(synchronize_session="fetch")
+                )
+                result = await session.execute(query)
+                try:
+                    await session.commit()
+                except SQLAlchemyError as e:
+                    await session.rollback()
+                    raise e
+                return result.rowcount
