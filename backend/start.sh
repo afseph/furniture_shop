6d26b3c1818db17aaf28a6c49f4b255e79b964fa
afseph <afseph@gmail.com>
2025-04-15 15:49:48 +0300

    ADD: Migrator service that allows to autmativly migrate DB

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Ждём БД..."
-while ! nc -z db 5432; do
-  sleep 1
-done
+# echo "Ждём БД..."
+# while ! nc -z db 5432; do
+#   sleep 1
+# done
 
 # echo "БД доступна. Генерируем миграцию."
 # # Уникальное имя ревизии по дате
 # REVISION_MSG="auto_$(date +%Y%m%d_%H%M%S)"
 # alembic revision --autogenerate -m "$REVISION_MSG"
 
-echo "Применяем миграции."
-alembic upgrade head
+# echo "Применяем миграции."
+# alembic upgrade head
 
 echo "Запускаем FastAPI."
 uvicorn app.main:app --host 0.0.0.0 --port 80
