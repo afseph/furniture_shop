888bb2dd817d71d3af2f0cc6e6852b044f2769fd
afseph <afseph@gmail.com>
2025-04-18 02:32:43 +0300

    FIX: Header profile/logout btns

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-// import "./Navbar.css"; // Можно подключить стили отдельно
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">Магазин Мебели</div>
       <ul className="navbar-links">
         <li><a href="/">Главная</a></li>
-        <li><a href="/login">Вход</a>/<a href="/register">Регистрация</a></li>
+        {isAuthenticated ? (
+          <>
+            <li><a href="/profile">Профиль</a> / <a href="/logout">Выход</a></li>
+          </>
+        ) : (
+          <li><a href="/login">Вход</a> / <a href="/register">Регистрация</a></li>
+        )}
         <li><a href="/contact">Контакты</a></li>
       </ul>
     </nav>
